46e8330e1e7094668538365e43ec58f35861c62b
Muqri Amin Bin Mohd Shamsuddin <muqri.amin@artrustees.com.my>
2026-02-18 04:56:13 +0800

    chore(docker): add database migration to container startup
    Run migrations automatically when the container starts to ensure the database schema is up to date. Using the `--force` flag allows it to run in non-interactive environments.

--- docker/start.sh
+++ docker/start.sh
@@ -6,5 +6,8 @@ php /var/www/html/artisan config:clear
 php /var/www/html/artisan cache:clear
 php /var/www/html/artisan route:clear
 
+# Run Migrations
+php /var/www/html/artisan migrate --force
+
 # Start Supervisord
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
